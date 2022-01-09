8cc7a49450a6a4a79856556dbd36ab6d8f0c2016
sano-jin <sano-jin@akane.waseda.jp>
2022-01-09 15:47:59 +0900

    プログレスメータを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+// import logo from "./logo.svg";
 // import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import ToggleButton from "@mui/material/ToggleButton";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import StopIcon from "@mui/icons-material/Stop";
+import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
 
 // 長さ length, で全ての値が initialValue な array を作る．
 // ただし，値は JSON.parse/stringfy を用いて deep copy する．
@@ -12,7 +16,7 @@ const makeArray = <T extends {}>(length: number, initialValue: T): T[] =>
   JSON.parse(JSON.stringify(Array(length).fill(initialValue)));
 
 // safari のサポートもするなら，webkitAudioContext も必要だが，面倒なのでやらない．
-const ctx = new AudioContext();
+const audioContext = new AudioContext();
 
 // 音源を取得し AudioBuffer 形式に変換して返す関数
 const setupSample = async (index: string) => {
@@ -20,7 +24,7 @@ const setupSample = async (index: string) => {
   const response = await fetch(`./SynthesizedPianoNotes/Piano${index}.mp3`);
   const arrayBuffer = await response.arrayBuffer();
   // Web Audio API で使える形式に変換
-  const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
   return audioBuffer;
 };
 
@@ -38,10 +42,6 @@ const scheduleAheadTime = 0.1; // How far ahead to schedule audio (in seconds)
 
 let currentNote = 0;
 let nextNoteTime = 0.0; // When the next note is due.
-
-// メインのトラック
-// 音を鳴らすタイミングは true
-const trackLength = 32;
 
 // 各音の名前
 const noteNames = [
@@ -76,6 +76,11 @@ const noteNames = [
   "C",
 ];
 
+// メインのトラックの長さ（todo: user definable にする）
+const trackLength = 32;
+
+// メインのトラック
+// 音を鳴らすタイミングは true
 let globalTracks: boolean[][] = makeArray(
   noteNames.length,
   Array(trackLength).fill(false)
@@ -89,9 +94,8 @@ const nextNote = () => {
 
   // Advance the beat number, wrap to zero
   currentNote++;
+
   // 最後まで到達したら最初に戻る
-  // （この例では，4 回鳴らすのをループする）
-  // if (currentNote === 4) {
   if (currentNote === trackLength) {
     currentNote = 0;
   }
@@ -120,14 +124,14 @@ const play = async (soundState: SoundState, time: number) => {
     return;
   }
 
-  // AudioBuffer を ctx に接続し再生する
-  const sampleSource = ctx.createBufferSource();
+  // AudioBuffer を audioContext に接続し再生する
+  const sampleSource = audioContext.createBufferSource();
 
   // 変換されたバッファを音源として設定
   sampleSource.buffer = audioBuffer;
 
   // 出力につなげる
-  sampleSource.connect(ctx.destination);
+  sampleSource.connect(audioContext.destination);
 
   // time 後に鳴らすようにスケジュールする
   // console.log("time: ", time);
@@ -159,48 +163,60 @@ const testSoundStates: SoundState[] = makeArray(noteNames.length, {
   isPlaying: false,
 });
 
+// 音源の名前（から，接頭辞の "Piano" を除いたもの）
+const soundFileNames = [
+  "11 (12)", // C
+  "11 (10)", // D
+  "11 (8)", // E
+  "11 (7)", // F
+  "11 (5)", // G
+  "11 (3)", // A
+  "11 (1)", // B
+  "11",
+  "13",
+  "15",
+  "16",
+  "18",
+  "110",
+  "112",
+  "113",
+  "115",
+  "117",
+  "118",
+  "120",
+  "122",
+  "124",
+  "125", // C
+  "125 (2)", // D
+  "125 (4)", // E
+  "125 (5)", // F
+  "125 (7)", // G
+  "125 (9)", // A
+  "125 (11)", // B
+  "125 (12)", // C
+];
+
+if (soundFileNames.length !== noteNames.length) {
+  throw new Error(
+    "implementation error: the number of the files and the notes are not the same"
+  );
+}
+
+let setupCounter = 0;
+
 // 音源を setup する．
 const setup = async () => {
-  [
-    "11 (12)", // C
-    "11 (10)", // D
-    "11 (8)", // E
-    "11 (7)", // F
-    "11 (5)", // G
-    "11 (3)", // A
-    "11 (1)", // B
-    "11",
-    "13",
-    "15",
-    "16",
-    "18",
-    "110",
-    "112",
-    "113",
-    "115",
-    "117",
-    "118",
-    "120",
-    "122",
-    "124",
-    "125", // C
-    "125 (2)", // D
-    "125 (4)", // E
-    "125 (5)", // F
-    "125 (7)", // G
-    "125 (9)", // A
-    "125 (11)", // B
-    "125 (12)", // C
-  ].forEach(async (note, index) => {
-    console.log("index, note", index, note);
-    const audioBuffer = await setupSample(note);
-    soundStates[index].audioBuffer = audioBuffer;
-    testSoundStates[index].audioBuffer = audioBuffer;
-  });
+  await Promise.all(
+    soundFileNames.map(async (note, index) => {
+      console.log("index, note", index, note);
+      const audioBuffer = await setupSample(note);
+      soundStates[index].audioBuffer = audioBuffer;
+      testSoundStates[index].audioBuffer = audioBuffer;
+    })
+  );
 
-  console.log(`done setup for tracks`);
+  console.log(`done setup for tracks`, ++setupCounter);
 };
-setup();
 
 const scheduleNote =
   (soundStates: SoundState[]) => (beatNumber: number, time: number) => {
@@ -227,7 +243,7 @@ let timerID: number = 0;
 const scheduler = () => {
   // while there are notes that will need to play before the next interval,
   // schedule them and advance the pointer
-  while (nextNoteTime < ctx.currentTime + scheduleAheadTime) {
+  while (nextNoteTime < audioContext.currentTime + scheduleAheadTime) {
     // console.log("scheduleNote");
     scheduleNote(soundStates)(currentNote, nextNoteTime);
     nextNote();
@@ -316,7 +332,7 @@ const Tracks = () => {
         fontSize: "calc(5px + 2vmin)",
         color: "#777",
         display: "block",
-        width: "90%",
+        width: "100%",
         maxHeight: "80vh",
         overflowY: "scroll",
       }}
@@ -355,11 +371,15 @@ const Tracks = () => {
   );
 };
 
-let lastNoteDrawn = 3;
+// アニメーションを行うための変数．
+// todo: closure に含めるようにしたほうが良いかも．
+let lastNoteDrawn = trackLength;
 
+// アニメーションを行う．
+// 現在再生中の箇所をハイライトする（影をつける）
 const draw = (setBeatTimer: (beatTimer: number) => void) => () => {
   let drawNote = lastNoteDrawn;
-  let currentTime = ctx.currentTime;
+  let currentTime = audioContext.currentTime;
 
   while (notesInQueue.length && notesInQueue[0].time < currentTime) {
     drawNote = notesInQueue[0].note;
@@ -392,6 +412,7 @@ const draw = (setBeatTimer: (beatTimer: number) => void) => () => {
   requestAnimationFrame(draw(setBeatTimer));
 };
 
+// 再生ボタン
 const PlayButton = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [beatTimer, setBeatTimer] = useState(0);
@@ -401,12 +422,12 @@ const PlayButton = () => {
       // start playing
 
       // check if context is in suspended state (autoplay policy)
-      if (ctx.state === "suspended") {
-        ctx.resume();
+      if (audioContext.state === "suspended") {
+        audioContext.resume();
       }
 
       currentNote = 0;
-      nextNoteTime = ctx.currentTime;
+      nextNoteTime = audioContext.currentTime;
       scheduler(); // kick off scheduling
       requestAnimationFrame(draw(setBeatTimer)); // start the drawing loop.
     } else {
@@ -433,13 +454,31 @@ const PlayButton = () => {
 };
 
 const App = () => {
-  useEffect(() => {});
+  // 音源をセットアップしている間は isLoading = true
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // 音源をセットアップする．
+    const loadFiles = async () => {
+      await setup();
+      setIsLoading(false);
+    };
+    loadFiles();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <PlayButton />
-        <Tracks />
+        {isLoading ? (
+          // 音源をセットアップしている間は，プログレスメータを回しておく
+          <CircularProgress color="secondary" />
+        ) : (
+          <div style={{ width: "90%", margin: "0 10%" }}>
+            <PlayButton />
+            <Tracks />
+          </div>
+        )}
       </header>
     </div>
   );
